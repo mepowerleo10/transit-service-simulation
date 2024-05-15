79a7e31953e409f9643bbfd8f3f7eb0947e9f5c5
mepowerleo10 <mepowerleo10@gmail.com>
2024-05-15 07:21:09 +0300

    extract variables

--- src/scenarios.py
+++ src/scenarios.py
@@ -12,6 +12,10 @@ class AbstractScenario:
         lambda_param: float,
         planning_horizon: float,
     ) -> None:
+        """
+        lambda_param: Demand density (number of passengers per hour per zone)
+        """
+
         self.num_of_zones_per_row = num_of_zones_per_row
         self.zone_length = zone_length
         self.lambda_param = lambda_param
@@ -22,15 +26,18 @@ class AbstractScenario:
 
         # Create a service region
         self.service_region = ServiceRegion(self.num_of_zones_per_row, self.zone_length)
-        
 
-    def run(self): ...
+        self.inbound_or_outbound_px = (
+            0.5  # Probability that a trip is inbound or outbound
+        )
+
+    def run(self, beta_values: List[float]): ...
 
     def save(self): ...
 
     def plot(self): ...
 
-    def initialize_random_trips(self, trips_density: int):
+    def generate_trips(self, trips_density: int):
         # Pick random stops from all other stops, excluding the fixed stop. Theses will be used to generate a list of trips
         random_rows_index = np.random.choice(
             self.service_region.none_fixed_stops.shape[0], trips_density
@@ -40,7 +47,7 @@ class AbstractScenario:
             reservation_time = np.random.randint(0, floor(self.planning_horizon * 60))
             direction_of_travel = (
                 TripDirection.INBOUND
-                if np.random.random() < 0.5
+                if np.random.random() < self.inbound_or_outbound_px
                 else TripDirection.OUTBOUND
             )
             trip = Trip(
@@ -70,5 +77,4 @@ class ScenarioZero(AbstractScenario):
         trips_density = int(
             self.service_region.num_of_zones * self.lambda_param * self.planning_horizon
         )
-        self.initialize_random_trips(trips_density)
-        
+        self.generate_trips(trips_density)
