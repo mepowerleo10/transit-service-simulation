4e22f7ff9c8d2039b8e7cf7a115555372ed81c81
mepowerleo10 <mepowerleo10@gmail.com>
2024-06-04 07:07:25 +0300

    Update the notebook  and environment configurations

--- src/env.py
+++ src/env.py
@@ -2,15 +2,15 @@ from pathlib import Path
 
 
 SECONDS_PER_MINUTE = 60
-RESERVATION_CUTOFF = 30
+RESERVATION_CUTOFF = 50
 OUTPUT_DIR = Path("output")
 
 NUMBER_OF_ZONES_PER_ROW = 10
-ZONE_LENGTH = 3
+ZONE_LENGTH = 10
 LAMBDA_PARAM = 0.5
 PLANNING_HORIZON = 0.9
 
-NUM_OF_SIMULATIONS = 1000
+NUM_OF_SIMULATIONS = 10_000
 SHUTTLE_SPEED = (
     0.20  # the shuttle moves with a constant speed of x units of distance/sec
 )
